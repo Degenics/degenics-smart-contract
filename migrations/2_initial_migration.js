60de059df0a73300d6dd92f4dc84e00253c661a5
Aloisius Dedy <aloisius.dedy@gmail.com>
2021-01-14 18:47:15 +0700

    update: add dummy addres for demo lab

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -225,22 +225,12 @@ module.exports = async function(deployer,network, accounts) {
 }
 
 async function dummyData(instances, accounts){
-    
+    var EthUtil = require('ethereumjs-util');
     try {
         const labList = await jsonfile.readFileSync('./migrations/listLab.json')
         if(accounts.length <= labList.length ){
             console.log('add account')
-            let _accounts =[
-                {address: '0x9739Aec67BfBE95a8e9228Ec056DF87a28037141', privateKey:'36fe83d7ed458c77f2f780ac47a753d4c7195ef34ce8b7fcca63b1276a7cf000'},
-                {address: '0x6C0F16DB088E52C8D58644201622db25Cd28B7Ee', privateKey:'d34e58ebf71a51e6d8db7590fb4e7d4a40f8b5b04a2c797863deee75aabe2d92' },
-                {address: '0x8101d5163ed98f71AcA2aefb590884e84cFdcD67', privateKey:'92c666f530c9939b476086d59539258040e7caa855d434d35a0611e135e5bc25'},
-                {address: '0x916eaB1B9AC0663803CA5Ef1a8aBb22108cFd19D', privateKey:'de7e610ddec992783523486bcc25ad6fdb945afed20b89e262fe73f29ceda70f'},
-                {address: '0x01288C996A36CA48D58181bAC99518ADCa0e2Bfc', privateKey:'c3018705fd876904c4d8eec83f3c92de29f16b7a3ab4de09a80d1508dc85cc03'},
-                {address: '0x066f152A2bA72A70ba86323feAC74061426A7bC1', privateKey:'aa8eaf52e17ea130ec9a5b832bee9133a34d97af103dd69fc8b54354fa7f8090'},
-                {address: '0xD833cdA13Ffef62276ebf10d091A8e35Fd2D5E00', privateKey:'aefa5e412b81f0a5d25aba48676cd7999b30b1b5c33196df18129b915b58d48b'},
-                {address: '0xa8B8F4a7783aa30113F2D843f19d629738B0F0ff', privateKey:'561c3b6dea3fbc9474e550d0006dd6932c7e0dd453bc991a941c9f5026d4c8ad'},
-                {address: '0x3D79AE783D2a769d8BbbFE4A726EC9879Da94576', privateKey:'fd1bc8129053d852b848f238b68b97b20d83e659a8e4d693ed65e67ebf8db930'},
-            ]
+            let _accounts = require('./dumpWallet')
             let count = labList.length - accounts.length
             for(let i = 0; i <= count; i++){
                 accounts.push(_accounts[i])
@@ -257,14 +247,17 @@ async function dummyData(instances, accounts){
             await instances.Lab.register(labAddress, lab.name, lab.country, lab.city); 
             if(labContract && (typeof accounts[i].privateKey != 'undefined') ){
                 console.log('add by web3')
+                
                 let account = await web3.eth.accounts
                                 .privateKeyToAccount(accounts[i].privateKey)
-                let pubKey  = '123'
+                let pubKey = accounts[i].publicKey ? accounts[i].publicKey : web3.utils.bytesToHex(EthUtil.privateToPublic(EthUtil.toBuffer(`${account.privateKey}`)))
                 let addData = JSON.stringify({address: `Jl. Nuri No.${i}`})
                 let data = labContract.methods.addAdditionalData(addData, pubKey).encodeABI()
                 await sendTransaction(instances.Lab.address, data, account)
                 for(let service of lab.services){
                     data = labContract.methods.registerService(service.code, service.serviceName, service.description, service.price).encodeABI()
+                    await sendTransaction(instances.Lab.address, data, account)
+                    data = labContract.methods.addServiceAdditionalData(service.code,JSON.stringify(service.additionalData)).encodeABI()
                     await sendTransaction(instances.Lab.address, data, account)
                 }
             }else if(labContract == null) {                
@@ -283,7 +276,7 @@ async function dummyData(instances, accounts){
             i++                
         }
     } catch (error) {
-        console.log('error')
+        console.log('error', error.message)
     }
 
 }
